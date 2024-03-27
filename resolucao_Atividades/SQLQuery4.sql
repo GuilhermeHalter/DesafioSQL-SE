440a4156c59b5f11ec0b8784b9b558cc2bd83794
Guilherme Halter Nunes <halter.guilhermenunes@gmail.com>
2024-03-27 13:43:10 -0300

    Add lista exercicios 4

---
+++ resolucao_Atividades/SQLQuery4.sql
@@ -0,0 +1,24 @@
+--LISTA 4 de Exercicios SQL
+USE treinamento
+
+--Faça um comando para listar:
+/*A) O nome de todos  que itens que ja foram pedidos (fazer uma versão com DISTINCT e uma com GROUP BY).*/
+
+/*B) O nome do produto, o numero de vezes que ele foi pedido e a quantidade total pedida;*/
+
+/*C) O nome do fornecedor, o total em estoque dos produtos fornecidos pelo fornecedor 
+a média de preço dos produtos fornecidos e quantos produtos ele fornece*/
+
+/*D) O nome do cliente, seu telefone, o valor da maior o valor da menor compra,
+o total comprado e a média de valor comprado ordenado por maior compra decrescente.*/
+
+/*E) A data do pedido, o nome do cliente, quandos proditos distintos ele pediu,
+o valor total do pedido(baseado nos valores do tabela productrequest), 
+amédia de valores do pedido (baseado nos valores da tabela productrequest) */
+
+/*F) O nome do fornecedor e quantos produtos ele fornece para tods os fornecedores que fornecem mais que um produto*/
+
+/*G) O nome do produto, o numero de vezes qu ele foi pedido e a quantidade total pedida para produtos que foram pedidos menos que 2 vezes*/
+
+/*H) O nome do cliente, o produto, o valor gasto com o produto, quantas vezes ele foi pedido pelo cliente e o nome do fornecedor. 
+Somente para produtos em que o cliente gastou mais de r$1000, ordenado por cliente e produto.*/
